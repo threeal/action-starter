65de2142a28ea5222f26140aab24676443276f76
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-17 13:49:08 +0700

    chore: remove distribution files (#58)
    * chore: remove `dist` directory
    
    * build: call `build` command during prepack
    
    * chore(git): ignore `package.tgz` file

--- dist/bin.mjs
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { mkdirRecursive } from "./mkdir.mjs";
-mkdirRecursive(process.argv[2]);
-//# sourceMappingURL=bin.mjs.map
--- dist/index.d.mts
+++
@@ -1,1 +0,0 @@
-export { mkdirRecursive } from "./mkdir.mjs";
