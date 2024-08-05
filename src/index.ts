68005c60dd82a0f068218bf852bd4c5222f769de
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-05 20:37:02 +0700

    feat: utilize catched-error-message to parse catched error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as core from "@actions/core";
+import { getErrorMessage } from "catched-error-message";
 import { mkdirRecursive } from "./mkdir.js";
 
 try {
   const path = core.getInput("path", { required: true });
   mkdirRecursive(path);
 } catch (err) {
-  core.setFailed(err);
+  core.setFailed(getErrorMessage(err));
 }
