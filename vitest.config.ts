d8fceaf3861e62b7241c34b8c85098868c12b6c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-12 11:42:11 +0700

    test: check test coverage on all files (#717)
    * test: remove `test.coverage.all` config in `vitest.config.ts`
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * test: simplify `test.coverage.reporter` config in `vitest.config.ts`
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      all: false,
       enabled: true,
-      reporter: ["text"],
+      reporter: "text",
       thresholds: { 100: true },
     },
   },
